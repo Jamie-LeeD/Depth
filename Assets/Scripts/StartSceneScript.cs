cdc59c3399eb5e5238f326480747b6856e974464
Jamie-LeeD <166004984+Jamie-LeeD@users.noreply.github.com>
2025-04-05 12:00:17 +0200

    Edited Mainmenu script

--- Assets/Scripts/StartSceneScript.cs
+++ Assets/Scripts/StartSceneScript.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    GameObject MainMenuUI;
+    GameObject OptionsUI;
+
+    private void Start()
+    {
+        OptionsUI.SetActive(false);
+    }
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -11,5 +18,17 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void OptionsButton()
+    {
+        OptionsUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        OptionsUI.SetActive(false);
+        MainMenuUI.SetActive(true);
     }
 }
