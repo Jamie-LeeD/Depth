a74f30dce8e2572f83f4ba4235063f1111d532b5
Jamie-LeeD <166004984+Jamie-LeeD@users.noreply.github.com>
2025-04-06 11:23:16 +0200

    Working on pause system

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject controlsPanel;
+    public GameObject SavePanel;
+    public GameObject LoadPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if(pausePanel.activeSelf) 
+            {
+                Continue(); 
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Continue()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Controls()
+    {
+        Debug.Log("clicked");
+        controlsPanel.SetActive(true);
+        SavePanel.SetActive(false);
+        LoadPanel.SetActive(false);
+    }
+
+    public void SaveBtn()
+    {
+        controlsPanel.SetActive(false);
+        SavePanel.SetActive(true);
+        LoadPanel.SetActive(false);
+    }
+
+    public void LoadBtn()
+    {
+        controlsPanel.SetActive(false);
+        SavePanel.SetActive(false);
+        LoadPanel.SetActive(true);
+    }
+}
