afda09100ced2541c8216a9137af1d2b99da730b
Jamie-LeeD <166004984+Jamie-LeeD@users.noreply.github.com>
2025-04-06 16:32:41 +0200

    Added HealthManager
    It should keep track of health between levels  and allow other scripts to access the isDead bool, allow access to take damage and display current health

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager Instance;
+
+    [SerializeField] private int maxHealth = 3;
+    private int currentHealth;
+    public bool isDead = false;
+
+
+    private void Awake()
+    {
+        // Singleton pattern: keep only one instance
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        if(isDead) return;
+
+        currentHealth--;
+        if (currentHealth <= 0) 
+        {
+            isDead = true;
+        }
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+        isDead = false;
+    }
+
+    public int getCurrentHealth()
+    {
+        return currentHealth;
+    }
+}
