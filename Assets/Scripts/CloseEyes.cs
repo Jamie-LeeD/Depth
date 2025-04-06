3bc9da2e0d812672dd1d658372a4bd80fe9d634b
DavidBarker05 <davidbarker9611@gmail.com>
2025-04-06 11:15:16 +0200

    Refined invisible path more

--- Assets/Scripts/CloseEyes.cs
+++ Assets/Scripts/CloseEyes.cs
@@ -1,34 +1,27 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CloseEyes : MonoBehaviour
 {
     [SerializeField]
-    GameObject eyelidsClosed;
+    Material eyelidsClosed;
     [SerializeField]
-    GameObject eyelidsOpened;
+    Material eyelidsOpened;
+    [SerializeField]
+    GameObject eyelids;
     [SerializeField]
     List<Light> sceneLights = new List<Light>();
 
     bool closed;
-
-    void Awake()
-    {
-        eyelidsClosed.SetActive(false);
-        eyelidsOpened.SetActive(true);
-    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
             closed = !closed;
-            eyelidsClosed.SetActive(closed);
-            eyelidsOpened.SetActive(!closed);
-            foreach (Light light in sceneLights)
-            {
-                light.gameObject.SetActive(!closed);
-            }
+            eyelids.GetComponent<MeshRenderer>().material = closed ? eyelidsClosed : eyelidsOpened;
+            Array.ForEach<Light>(sceneLights.ToArray(), l => l.gameObject.SetActive(!closed));
         }
     }
 }
